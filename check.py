d989fac3bb48f7c1f44e53d9e285d0f1c9dbb564
femaffi13 <70483893+femaffi13@users.noreply.github.com>
2025-11-07 19:19:42 -0300

    req

--- check.py
+++ check.py
@@ -1,11 +1,31 @@
 import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+chrome_options = Options()
+service = Service(ChromeDriverManager().install()) # Configurar Selenium sin descargar el driver manualmente
+driver = webdriver.Chrome(service=service, options=chrome_options)
+driver.maximize_window() #Maximizar la ventana
+
+#driver.get("https://www.allaccess.com.ar/event/acdc-venta-general") #Abrir la página
+driver.get("https://www.allaccess.com.ar/event/airbag") #Abrir la página
+
+time.sleep(3) #Esperar unos segundos para que cargue la página
 
 def enviar_telegram(mensaje):
     token = '7785456335:AAFQCxNkifYm8teUrRgRYda8m5Mf4YV_gN0'
     chat_id = '1396394457'
+
     url = f'https://api.telegram.org/bot{token}/sendMessage'
-    payload = {'chat_id': chat_id, 'text': mensaje}
+    payload = {
+        'chat_id': chat_id,
+        'text': mensaje
+    }
     try:
         response = requests.post(url, data=payload)
         if response.status_code == 200:
@@ -15,26 +35,12 @@ def enviar_telegram(mensaje):
     except Exception as e:
         print("⚠️ Excepción al enviar Telegram:", e)
 
-# URL del evento
-url = "https://www.allaccess.com.ar/event/acdc-venta-general"
-
-# Obtener el HTML
 try:
-    response = requests.get(url, timeout=10)
-    response.raise_for_status()
-    print("Código de estado:", response.status_code)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    print(response.text[:40000])  # Muestra los primeros 2000 caracteres
+    soldout_div = driver.find_element(By.CLASS_NAME, "event-status.status-soldout")
+    print("🎟️ El evento está agotado.")
+except NoSuchElementException:
+    print("✅ El evento NO está marcado como agotado.")
+    enviar_telegram("🎉 ¡El evento ACDC NO está agotado! Revisá AllAccess.")
 
-
-    # Buscar el div con clase "event-status status-soldout"
-    soldout_div = soup.find("div", lambda tag: tag.get("class") and "status-soldout" in tag.get("class"))
-    print(soldout_div)
-
-    if soldout_div:
-        print("🎟️ El evento está agotado.")
-    else:
-        print("✅ El evento NO está marcado como agotado.")
-        #enviar_telegram("🎉 ¡El evento ACDC NO está agotado! Revisá AllAccess.")
-except Exception as e:
-    print("⚠️ Error al acceder a la página:", e)
+# Cerrar el navegador
+driver.quit()
