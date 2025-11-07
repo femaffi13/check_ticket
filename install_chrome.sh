adc4692c7a07a1ef3f535b3d464b110fff9c3b62
femaffi13 <70483893+femaffi13@users.noreply.github.com>
2025-11-07 19:57:29 -0300

    chronium

--- install_chrome.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt update
-apt install -y ./google-chrome-stable_current_amd64.deb
---
+++ install_chronium.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#!/bin/bash
+set -e
+
+apt-get update
+apt-get install -y chromium-browser
