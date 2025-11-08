21ed0107342cdbcbd42c3252938da1381ac6566c
femaffi13 <70483893+femaffi13@users.noreply.github.com>
2025-11-08 11:32:57 -0300

    acdc

---
+++ check_2.py
@@ -0,0 +1,50 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+import time
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+service = Service(ChromeDriverManager().install()) # Configurar Selenium sin descargar el driver manualmente
+driver = webdriver.Chrome(service=service, options=chrome_options)
+driver.maximize_window() #Maximizar la ventana
+
+driver.get("https://www.allaccess.com.ar/event/acdc-venta-general") 
+#driver.get("https://www.allaccess.com.ar/event/airbag") 
+
+time.sleep(5) #Esperar unos segundos para que cargue la página
+
+def enviar_telegram(mensaje):
+    token = '7785456335:AAFQCxNkifYm8teUrRgRYda8m5Mf4YV_gN0'
+    chat_id = '1396394457'
+
+    url = f'https://api.telegram.org/bot{token}/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': mensaje
+    }
+    try:
+        response = requests.post(url, data=payload)
+        if response.status_code == 200:
+            print("📲 Mensaje enviado por Telegram.")
+        else:
+            print("❌ Error al enviar mensaje:", response.text)
+    except Exception as e:
+        print("⚠️ Excepción al enviar Telegram:", e)
+
+try:
+    soldout_div = driver.find_element(By.CLASS_NAME, "event-status.status-soldout")
+    print("🎟️ El evento está agotado.")
+    enviar_telegram("Evento agotado ❌")
+except NoSuchElementException:
+    print("✅ El evento NO está marcado como agotado.")
+    enviar_telegram("AC⚡DC 🎉 https://www.allaccess.com.ar/event/acdc-venta-general")
+
+# Cerrar el navegador
+driver.quit()
